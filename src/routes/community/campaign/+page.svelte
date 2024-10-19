2324541999af244600c12c79c65dabc32e7cf3b2
TheSisyphean <thesisyphean256@gmail.com>
2024-10-19 18:41:00 +0200

    Adding data

--- src/routes/community/campaign/+page.svelte
+++ src/routes/community/campaign/+page.svelte
@@ -5,7 +5,15 @@
     export let campaignName = "Campaign";
     export let description = "Description...";
     export let percentFunded = 55;
-    export let isOwner = false;
+    export let isOwner = true;
+    export let loans = [
+        {
+            title: "Loan 1",
+            beneficiary: "Joe Biden",
+            totalAmount: 1000,
+            amountPaid: 500,
+        }
+    ];
 </script>
 
 <div class="bg-base-200 min-h-screen p-10 space-y-10">
