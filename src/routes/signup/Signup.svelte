006fba36e52d3c491531eafea982cfb66d89ae13
TheSisyphean <thesisyphean256@gmail.com>
2024-10-20 00:22:33 +0200

    Fixed all forms and completed signup

--- src/routes/signup/Signup.svelte
+++ src/routes/signup/Signup.svelte
@@ -1,7 +1,7 @@
 <div class="card bg-base-100 shadow-xl">
   <div class="card-body">
     <h2 class="card-title text-2xl font-bold">Signup</h2>
-    <form>
+    <form method="post">
 
       <div class="grid grid-cols-2 gap-4">
         <div class="form-control">
@@ -9,7 +9,7 @@
             <span class="label-text">First Name</span>
           </label>
           <label class="input input-bordered flex items-center gap-2">
-            <input type="text" class="grow" placeholder="John" />
+            <input name="firstName" type="text" class="grow" placeholder="John" />
           </label>
         </div>
         <div class="form-control">
@@ -17,7 +17,7 @@
             <span class="label-text">Last Name</span>
           </label>
           <label class="input input-bordered flex items-center gap-2">
-            <input type="text" class="grow" placeholder="Doe" />
+            <input name="lastName" type="text" class="grow" placeholder="Doe" />
           </label>
         </div>
         <div class="form-control">
@@ -25,7 +25,7 @@
             <span class="label-text">Email</span>
           </label>
           <label class="input input-bordered flex items-center gap-2">
-            <input type="email" class="grow" placeholder="email@example.com" />
+            <input name="email" type="email" class="grow" placeholder="email@example.com" />
           </label>
         </div>
         <div class="form-control">
@@ -33,7 +33,7 @@
             <span class="label-text">Password</span>
           </label>
           <label class="input input-bordered flex items-center gap-2">
-            <input type="password" class="grow" placeholder="Enter password" />
+            <input name="password" type="password" class="grow" placeholder="Enter password" />
           </label>
         </div>
       </div>
@@ -42,11 +42,11 @@
             <span class="label-text">Wallet URL</span>
           </label>
           <label class="input input-bordered flex items-center gap-2">
-            <input type="text" class="grow" placeholder="Enter wallet address" />
+            <input name="url" type="text" class="grow" placeholder="Enter wallet address" />
           </label>
         </div>
       <div class="form-control mt-6">
-        <a href="community" class="btn btn-primary"> Signup </a>
+        <button type="submit" class="btn btn-primary"> Signup </button>
       </div>
     </form>
   </div>
