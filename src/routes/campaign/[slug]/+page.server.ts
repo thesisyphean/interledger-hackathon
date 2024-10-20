8bb1313799f1b76a16ebe15b81ec27b105103b66
Andre Toerien <andre.toerien8@gmail.com>
2024-10-20 04:44:11 +0200

    Fix campaign page

--- src/routes/campaign/[slug]/+page.server.ts
+++ src/routes/campaign/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
-import type { PageServerLoad } from '../$types';
-import { getCampaignById, getCommunityCampaignsById } from "$lib/server/database/campaign";
+import type { PageServerLoad } from "./$types";
+import { getCampaignById } from "$lib/server/database/campaign";
 import { error, redirect } from "@sveltejs/kit";
 import { check_session } from "$lib/server/sessions";
 import { getUserById } from "$lib/server/database/users";
@@ -11,21 +11,21 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
   const user = await getUserById(userUuid);
   if (!user) return redirect(303, "/login");
 
-  const campaign = await getCommunityCampaignsById(params.slug);
+  const campaign = await getCampaignById(params.slug);
   if (!campaign) error(404);
 
   return {
-      campaignName: campaign.name,
-      description: campaign.description,
-      requiredAmount: campaign.amount,
-      isOwner: campaign.userId === user.userId,
-      loans: [
-          {
-              title: "Loan 1",
-              beneficiary: "Joe Soap",
-              totalAmount: 1000,
-              amountPaid: 500,
-          }
-      ]
+    campaignName: campaign.name,
+    description: campaign.description,
+    requiredAmount: campaign.amount,
+    isOwner: campaign.userId === user.userId,
+    loans: [
+      {
+        title: "Loan 1",
+        beneficiary: "Joe Soap",
+        totalAmount: 1000,
+        amountPaid: 500,
+      },
+    ],
   };
-}
+};
