5c634f109c32435c111036d698b2f2a77ee0c2d4
TheSisyphean <thesisyphean256@gmail.com>
2024-10-20 07:35:20 +0200

    Added no campaigns message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,15 @@
 
 <Navbar communities={data.communities} />
 <div class="flex flex-col pt-10 space-y-10 bg-base-200 min-h-screen">
+    {#if data.campaigns.length}
     {#each {length: data.campaigns.length / 3 + 1} as _, i}
-    <!-- TODO -->
     <div class="flex justify-center items-center space-x-16">
         {#each data.campaigns.slice(i * 3, i * 3 + 3) as campaign}
         <CampaignCard {...campaign} />
         {/each}
     </div>
     {/each}
+    {:else}
+    <h1 class="w-full text-center text-xl">No Campaigns</h1>
+    {/if}
 </div>
